ac8be6c096a7249b7897e3970776a193833fac8c
mamuka <mamuka.mirinahvili.1@iliauni.edu.ge>
2023-02-15 23:28:54 +0400

    react project

---
+++ javascript/lecture-10/app.js
@@ -0,0 +1,29 @@
+
+// დავალება #1
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', function () {
+//     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    
+//     btn.style.backgroundColor = randomColor
+// })
+
+
+// დავალება #2
+
+
+// const btn = document.querySelectorAll('#mybtn');
+
+// btn.forEach(element => {
+//     element.addEventListener('click', function (e) {
+
+//         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+//         element.style.background = randomColor
+
+//         console.log(e);
+   
+//     })
+      
+// });
+
